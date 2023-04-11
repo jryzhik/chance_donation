2ba3dc07099b15f84c5d3a6cd78e4a34040d8bc0
jryzhik <jamesryzhkov@gmail.com>
2023-04-11 14:27:50 -0400

    Remove all edge associations

--- next.config.js
+++ next.config.js
@@ -3,12 +3,5 @@ module.exports = {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
-    },
-    experimental: {
-        images: {
-            unoptimized: true
-        },
-        runtime: 'edge',
-    },
-    reactStrictMode: true,
+    }
   }
--- pages/thanks/index.js
+++ pages/thanks/index.js
@@ -1,9 +1,5 @@
 import BrainThanks from '../../src/components/brainThanks';
 
-export const config = {
-  runtime: 'edge',
-}
-
 function Thanks({ id }) {
   return (
       <BrainThanks id = {id}/>
