a209eac46fe255074741f3af51a18a80b4f966b6
jryzhik <jamesryzhkov@gmail.com>
2023-04-11 14:24:45 -0400

    Try to add cloud edge

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ module.exports = {
     experimental: {
         images: {
             unoptimized: true
-        }
-    }
+        },
+        runtime: 'edge',
+    },
+    reactStrictMode: true,
   }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  runtime: 'edge',
+}
+
+export default async function handler(req) {
+  return new Response(JSON.stringify({ name: 'John Doe' }))
 }
