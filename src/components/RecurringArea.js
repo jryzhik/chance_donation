5bb9d1dfade21f6b99e59b032f2cad794109992e
jryzhik <jamesryzhkov@gmail.com>
2024-08-19 12:46:10 -0400

    bronze 5 doll month addition

--- src/components/RecurringArea.js
+++ src/components/RecurringArea.js
@@ -6,10 +6,16 @@ export default function RecurringArea() {
             <script async
                 src="https://js.stripe.com/v3/buy-button.js">
             </script>
-            <div style={{display: "flex", justifyContent: "center", paddingTop: "3em"}}>
+            <div style={{ display: "flex", justifyContent: "center", paddingTop: "3em" }}>
                 <h3>Weekly Donation</h3>
             </div>
             <div className='subscriptionItems'>
+                <stripe-buy-button
+                    buy-button-id="buy_btn_1PopUqHCi0SX3Bt1kw0H0NoA"
+                    publishable-key="pk_live_51IQz0aHCi0SX3Bt1kcGe8R5aga19OLfNENQPcs5qBQ9KHBg3usTNLdnDC1jUdQi2EOEM0cyyZiyOkHcDnY1ddb4t00juHVL3qR"
+                >
+                </stripe-buy-button>
+
                 <stripe-buy-button
                     buy-button-id="buy_btn_1NjNTPHCi0SX3Bt1NEE1nuWD"
                     publishable-key="pk_live_51IQz0aHCi0SX3Bt1kcGe8R5aga19OLfNENQPcs5qBQ9KHBg3usTNLdnDC1jUdQi2EOEM0cyyZiyOkHcDnY1ddb4t00juHVL3qR"
