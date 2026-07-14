bd96d37f73ae1fc280d530dacf2fa4646e4c60fd
jryzhik <jamesryzhkov@gmail.com>
2026-07-13 22:20:09 -0400

    mobile menu add portal

--- src/layouts/MobileMenu.js
+++ src/layouts/MobileMenu.js
@@ -40,6 +40,9 @@ const MobileMenu = () => {
               <a href="#donate">Donate</a>
             </li>
             <li>
+              <a href="https://billing.stripe.com/p/login/5kQcN59q8cVr6r7f8T7EQ00">Portal</a>
+            </li>
+            <li>
               <a href="#gallery">Gallery</a>
             </li>
             <li>
